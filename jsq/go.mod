d2101d251970279d87b0c5a3afe75d2798519514
Thomas Abraham <thomasabraham@users.noreply.github.com>
2020-06-28 06:55:29 +0200

    Using parser

--- jsq/go.mod
+++ jsq/go.mod
@@ -1,3 +1,9 @@
 module github.com/thomasabraham/JSONStreamQuery/jsq
 
 go 1.13
+
+replace github.com/thomasabraham/JSONStreamQuery/query => ../query
+
+require github.com/antlr/antlr4 v0.0.0-20200503195918-621b933c7a7f
+
+require github.com/thomasabraham/JSONStreamQuery/query v0.0.0
--- jsq/jsq.go
+++ jsq/jsq.go
@@ -1,5 +1,10 @@
 package jsq
 
+import (
+	"github.com/thomasabraham/JSONStreamQuery/query/parser"
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+)
+
 type JSONStreamQuerier interface {
 	Query(input []interface{}) []interface{}
 }
@@ -7,7 +12,22 @@ type JSONStreamQuerier interface {
 type JSONStreamQuery struct {
 }
 
+type queryListener struct {
+	*parser.BasequeryListener
+}
+
 func BuildJSONStreamQuerier(query string) JSONStreamQuerier {
+	is := antlr.NewInputStream(query)
+	// Create the Lexer
+	lexer := parser.NewqueryLexer(is)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	// Create the Parser
+	p := parser.NewqueryParser(stream)
+
+	// Finally parse the expression
+	antlr.ParseTreeWalkerDefault.Walk(&queryListener{}, p.Start())
+
 	return JSONStreamQuery{}
 }
 
